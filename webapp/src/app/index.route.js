b9f5a81dedb0887ab96d554e57f97def4ed887a3
Guillaume Moigneu <n@nls.io>
2016-03-06 15:30:42 +0100

    Add Author & Module controllers & routing

--- webapp/src/app/index.route.js
+++ webapp/src/app/index.route.js
@@ -26,8 +26,19 @@
         templateUrl: 'app/grid/grid.html',
         controller: 'GridController',
         controllerAs: 'grid'
+      })
+      .state('module', {
+        url: '/module/:moduleId',
+        templateUrl: 'app/module/module.html',
+        controller: 'ModuleController',
+        controllerAs: 'module'
+      })
+      .state('author', {
+        url: '/author/:authorId',
+        templateUrl: 'app/author/author.html',
+        controller: 'AuthorController',
+        controllerAs: 'author'
       });
-
     $urlRouterProvider.otherwise('/');
   }
 
---
+++ webapp/src/app/module/module.controller.js
@@ -0,0 +1,22 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('magerepository')
+        .controller('ModuleController', ModuleController)
+    ;
+
+    /** @ngInject */
+    function ModuleController($log, $stateParams, Restangular) {
+        var vm = this;
+
+        Restangular.one('modules', $stateParams.moduleId).get().then(function(module) {
+            vm.module = module;
+            vm.author = module.author.data;
+            $log.info(vm.module);
+        });
+
+    }
+
+})();
+
